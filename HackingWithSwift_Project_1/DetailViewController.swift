cc1a614f5bff745a9a19e9f05f9d54663fd37d5f
Anthony Lee <anthonylee@umass.edu>
2019-03-31 13:26:09 -0400

    hidesBarsOnTap and PrefersLargeTites properties for navigation controller

--- HackingWithSwift_Project_1/DetailViewController.swift
+++ HackingWithSwift_Project_1/DetailViewController.swift
@@ -9,13 +9,30 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
     @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if let selectedImage = selectedImage {
+            imageView.image = UIImage(named: selectedImage)
+        }
+        
+        navigationItem.title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
     }
     
 
