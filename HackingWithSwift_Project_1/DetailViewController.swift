27e8bc149973f84f41ada998e724c939ad68efc6
Anthony Lee <anthonylee@umass.edu>
2019-03-31 13:03:01 -0400

    IBOutlet connections

---
+++ HackingWithSwift_Project_1/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  HackingWithSwift_Project_1
+//
+//  Created by Anthony Lee on 3/31/19.
+//  Copyright © 2019 anthonyLee. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
